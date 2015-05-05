ef80bba0840071534b066f104088a4568b4e69df
mambax7 <mambax7@gmail.com>
2015-05-05 09:39:40 -0400

    Original files

---
+++ xmgg/txc_themeinfo.php
@@ -0,0 +1,6 @@
+<?php
+	$txc_themeinfo["name"] = "RMSOFT XMGG";
+	$txc_themeinfo["author"] = "Red M¨¦xico Soft";
+	$txc_themeinfo["author_url"] = "http://www.redmexico.com.mx/";
+	$txc_themeinfo["detail_url"] = "http://www.xoops-mexico.net/modules/rmdp/down.php?id=42";
+?>
